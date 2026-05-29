fcca3588ad1996160b63898e0e3a05c757b9fc2e
fairus0305 <ruszezzy@gmail.com>
2026-05-29 17:42:45 +0700

    menambah fungsi tombol 3

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -154,3 +154,9 @@ int main()
                 q.remove();
                 break;
             }
+
+            case '3':
+            {
+                q.display();
+                break;
+            }
